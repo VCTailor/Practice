d36c6b5dfd5c5e75b53bbb95fdae41c1d046d341
Varsha Tailor <tailorvc@gmail.com>
2016-04-02 14:51:43 +0100

    added new line of code into java class

--- src/TestPractice.java
+++ src/TestPractice.java
@@ -2,5 +2,6 @@ public void TestPractice{
 	public static void main(String args[]){
 		System.out.println("Hellow");
 		System.out.println("Edited form Git");
+		System.out.println("Clonnig it from the command line");
 	}
 }
